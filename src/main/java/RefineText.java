156c3244acc62278bd5716c5bdc541fae84690c9
Cheolho Jeon <woung2770@gmail.com>
2022-03-04 18:14:33 +0900

    Refactoring RefineText

--- src/main/java/RefineText.java
+++ src/main/java/RefineText.java
@@ -1,7 +1,13 @@
 public class RefineText {
 
     public String refineText(String s) {
-        return s.replace("    ", " ").replace("  ", " ");
+        return s.replace("    ", " ")
+            .replace("\t", " ")
+            .replace("  ", " ")
+            .replace("  ", " ")
+            .replace("  ", " ")
+            .replace("mockist", "*******")
+            .replace("purist", "******");
     }
 
 }
--- src/test/java/RefineTextTest.java
+++ src/test/java/RefineTextTest.java
@@ -1,9 +1,12 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -11,44 +14,44 @@ public class RefineTextTest {
 
     private RefineText refineTextInstance = new RefineText();
 
-//    @Test
-//    @DisplayName("sut transforms \"hello  world\" to \"hello world\"")
-//    void test1() {
-//        String actual = refineTextInstance.refineText("hello  world");
-//        assertEquals("hello world", actual);
-//    }
-//
-//    @Test
-//    @DisplayName("sut transforms \"hello    world\" to \"hello world\"")
-//    void test2() {
-//        String actual = refineTextInstance.refineText("hello    world");
-//        assertEquals("hello world", actual);
-//    }
-//
-//    @Test
-//    @DisplayName("sut transforms \"hello   world\" to \"hello world\"")
-//    void test3() {
-//        String actual = refineTextInstance.refineText("hello   world");
-//        assertEquals("hello world", actual);
-//    }
-//
-//    @Test // 여러 입력 중, 어떤 입력에 대해 테스트가 실패했는지 알 수 없음
-//    @DisplayName("sut correctly works")
-//    void testWithLooping() {
-//        List<String> sources = List.of("hello  world", "hello    world", "hello   world");
-//        for (String source : sources) {
-//            String actual = refineTextInstance.refineText(source);
-//            assertEquals("hello world", actual);
-//        }
-//    }
-
     // 각 입력에 대한 테스트 결과를 출력해줌
     // 단, JUnit에서 여러 공백 문자가 있을 시에 공백 처리를 해줘서 테스트 결과에는 다르게 표기되지 않는 문제가 있음
     @ParameterizedTest
-    @ValueSource(strings = { "hello  world", "hello    world", "hello   world" })
+    @ValueSource(strings = {
+        "hello  world",
+        "hello   world",
+        "hello    world",
+        "hello     world",
+        "hello      world",
+        "hello       world"
+    })
     void testWithParameterizedTest(String source) {
         String actual = refineTextInstance.refineText(source);
         assertEquals("hello world", actual);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "hello\t world",
+        "hello \tworld",
+    })
+    void testAboutTabCharacter(String source) {
+        String actual = refineTextInstance.refineText(source);
+        assertEquals("hello world", actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideStringsForIsBlank")
+    void testAboutBannedWords(String source, List<String> bannedWords, String expected) {
+        String actual = refineTextInstance.refineText(source);
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> provideStringsForIsBlank() {
+        return Stream.of(
+            Arguments.of("hello mockist", List.of("mockist", "purist"), "hello *******"),
+            Arguments.of("hello purist", List.of("mockist", "purist"), "hello ******")
+        );
     }
 
 }
