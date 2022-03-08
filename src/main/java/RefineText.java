ba925add45c603cce6342176ed94935624c00392
Cheolho Jeon <woung2770@gmail.com>
2022-03-08 16:38:18 +0900

    Applying @MethodSrouce, Faker

--- src/main/java/RefineText.java
+++ src/main/java/RefineText.java
@@ -1,13 +1,18 @@
 public class RefineText {
 
-    public String refineText(String s) {
-        return s.replace("    ", " ")
+    public String refineText(String s, String ... bannedWords) {
+        s = s.replace("    ", " ")
             .replace("\t", " ")
             .replace("  ", " ")
             .replace("  ", " ")
             .replace("  ", " ")
             .replace("mockist", "*******")
             .replace("purist", "******");
+
+        for (String bannedWord : bannedWords) {
+            s = s.replace(bannedWord, "*".repeat(bannedWord.length()));
+        }
+        return s;
     }
 
 }
--- src/test/java/RefineTextTest.java
+++ src/test/java/RefineTextTest.java
@@ -1,4 +1,4 @@
-import org.junit.jupiter.api.DisplayName;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -13,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class RefineTextTest {
 
     private RefineText refineTextInstance = new RefineText();
+    private Faker faker = new Faker();
 
     // 각 입력에 대한 테스트 결과를 출력해줌
     // 단, JUnit에서 여러 공백 문자가 있을 시에 공백 처리를 해줘서 테스트 결과에는 다르게 표기되지 않는 문제가 있음
@@ -41,17 +42,28 @@ public class RefineTextTest {
     }
 
     @ParameterizedTest
-    @MethodSource("provideStringsForIsBlank")
+    @MethodSource("provideStringsForBannedWord")
     void testAboutBannedWords(String source, List<String> bannedWords, String expected) {
         String actual = refineTextInstance.refineText(source);
         assertEquals(expected, actual);
     }
 
-    private static Stream<Arguments> provideStringsForIsBlank() {
+    private static Stream<Arguments> provideStringsForBannedWord() {
         return Stream.of(
             Arguments.of("hello mockist", List.of("mockist", "purist"), "hello *******"),
             Arguments.of("hello purist", List.of("mockist", "purist"), "hello ******")
         );
+    }
+
+    @Test
+    void testAboutGeneratedBannedWords() {
+        String bannedWord = faker.lorem().word();
+        String source = "hello " + bannedWord;
+
+        String expected = "hello " + "*".repeat(bannedWord.length());
+        String actual = refineTextInstance.refineText(source, bannedWord);
+
+        assertEquals(expected, actual);
     }
 
 }
